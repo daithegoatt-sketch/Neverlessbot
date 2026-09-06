51cfc45b7f51a1f2e93c6eeba71eb8488c1b35b4
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-07 00:37:21 +0300

    Enable hidden achievements and rewards

--- src/genshin/preload.js
+++ src/genshin/preload.js
@@ -24,6 +24,7 @@ const { installPublicGenshinCommands, isPublicGenshinCommand } = require('./publ
 const { installModeration } = require('../moderation');
 const { installDelegatedMessageDelete } = require('../delegatedMessageDelete');
 const { installActivity } = require('../activityV2');
+const { installHiddenAchievements } = require('../hiddenAchievementsV3');
 const { installServerTools } = require('../serverTools');
 const { installTempVoicePersistence } = require('../tempVoicePersistence');
 const { installAutoMod } = require('../autoMod');
@@ -115,6 +116,7 @@ Client.prototype.login = function neverlessGenshinLogin(token) {
     installModeration(this);
     installDelegatedMessageDelete(this);
     installActivity(this);
+    installHiddenAchievements(this);
     installNicknameCommand(this);
     installGeneralChatGreetings(this);
     installTempVoicePersistence(this);
