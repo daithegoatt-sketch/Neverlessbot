3df8464a55b09139728f39df83ab7dcadfbb4cc6
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-07 01:36:30 +0300

    Install Neverless AI without changing existing routers

--- src/genshin/preload.js
+++ src/genshin/preload.js
@@ -32,6 +32,7 @@ const { installAntiRaid } = require('../antiRaid');
 const { installGameLobby } = require('../gameLobby');
 const { installNicknameCommand } = require('../nicknameCommand');
 const { installGeneralChatGreetings } = require('../generalChatGreetings');
+const { installNeverlessAI } = require('../neverlessAI');
 
 const CHANNEL_ID = process.env.GENSHIN_CHANNEL_ID || '1538091335079297034';
 const TEST_CHANNEL_ID = process.env.GENSHIN_TEST_CHANNEL_ID || '1539226931319545936';
@@ -117,6 +118,7 @@ Client.prototype.login = function neverlessGenshinLogin(token) {
     installDelegatedMessageDelete(this);
     installActivity(this);
     installHiddenAchievements(this);
+    installNeverlessAI(this);
     installNicknameCommand(this);
     installGeneralChatGreetings(this);
     installTempVoicePersistence(this);
