9e01a2c3e5ec9d2ec75755bbb40ecfc9d67fc441
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-08 02:30:10 +0300

    Route main and music service startup

---
+++ start.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { spawnSync } = require('node:child_process');
+
+const mode = String(process.env.BOT_MODE || 'main').trim().toLowerCase();
+
+if (mode === 'music') {
+  require('./src/music/worker');
+} else {
+  const prepare = spawnSync(process.execPath, ['prepare.js'], { stdio: 'inherit' });
+  if (prepare.status !== 0) process.exit(prepare.status || 1);
+  require('./src/genshin/preload');
+  require('./index');
+}
