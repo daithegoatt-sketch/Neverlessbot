ef98fbc2ce588af7a698903c9affed2220e97b64
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-08 06:15:20 +0300

    Switch isolated music workers to Lavalink backend

--- src/music/resetWorker.js
+++ src/music/resetWorker.js
@@ -2,4 +2,4 @@
 
 // BOT_MODE=music starts only the isolated music worker process.
 // User commands are handled by the main Neverless bot; workers only consume the private music bus.
-require('./workerDirectBusV2');
+require('./workerLavalinkV4');
