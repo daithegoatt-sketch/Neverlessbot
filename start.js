ca480f761b3e5eae879307fd36a6629a858ff197
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-08 03:56:58 +0300

    Use hardened Lavalink music worker

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const { spawnSync } = require('node:child_process');
 const mode = String(process.env.BOT_MODE || 'main').trim().toLowerCase();
 
 if (mode === 'music') {
-  require('./src/music/workerLavalink');
+  require('./src/music/workerLavalinkV2');
 } else {
   const prepare = spawnSync(process.execPath, ['prepare.js'], { stdio: 'inherit' });
   if (prepare.status !== 0) process.exit(prepare.status || 1);
