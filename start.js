f691583fa959786e8a4ab6908f325004bd374ac7
daithegoatt-sketch <daithegoatt@gmail.com>
2026-09-10 23:37:30 +0300

    Load isolated voice leaderboard in main bot

--- start.js
+++ start.js
@@ -9,6 +9,7 @@ if (mode === 'music') {
 } else {
   const prepare = spawnSync(process.execPath, ['prepare.js'], { stdio: 'inherit' });
   if (prepare.status !== 0) process.exit(prepare.status || 1);
+  require('./src/voiceTopHook');
   require('./src/genshin/preload');
   require('./src/music/mainControllerHook');
   require('./index');
